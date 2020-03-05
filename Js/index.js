517915a592943c7048177f765ab920860ec5412b
Siddhartha Raju <raju.s@husky.neu.edu>
2020-03-05 18:35:20 -0500

    Added delete column and button listener

--- Js/index.js
+++ Js/index.js
@@ -11,11 +11,12 @@ function onLoad()
     window.rowCount=1;
     window.columnCount='a';
     window.table = document.getElementById("table");
-    quoDelete();
+    
+    window.initialSetup();
     window.table.addEventListener("click", function()
         {
             //alert("Event happened") //Test if event is working by uncommenting the command alert
-            quoDelete();
+            quoButton();
         }
         )
     document.getElementById("rowCheck").innerHTML = "Row index is: " + window.rowIndex;
@@ -23,12 +24,24 @@ function onLoad()
 
 }
 
+function initialSetup()
+{
+    for(let i=1;i<15;i++)
+    {
+        window.addRow();
+    }
+    for(let i=0;i<15;i++)
+    {
+        window.addColumn();
+    }
+}
 
-
-function quoDelete()
+function quoButton()
 {
     let del = document.getElementById('btnDelteRow');
-    if(window.rowIndex>=1 && window.table.rows.length>2)
+    let delComun = document.getElementById('btnDelCol');
+    let addRow = document.getElementById('btnAddRow');
+    if(window.table.rows.length>2)
     {
         //alert("Disabled");
         del.disabled=false;
@@ -36,6 +49,26 @@ function quoDelete()
     else
     {
         del.disabled=true;
+    }
+    
+    if(window.table.rows[0].cells.length>2)
+    {
+        //alert("Disabled");
+        delComun.disabled=false;
+    }
+    else
+    {
+        delComun.disabled=true;
+    }
+
+    if(window.table.rows.length<21)
+    {
+        //alert("Disabled");
+        addRow.disabled=false;
+    }
+    else
+    {
+        addRow.disabled=true;
     }
 }
 
@@ -68,6 +101,7 @@ function addRow() {
     window.rowIndex = -1;
     window.columnIndex = -1;
     window.updateRowNumber();
+    window.quoButton();
   }
 
 
@@ -81,17 +115,23 @@ function addRow() {
     row= window.table.deleteRow(window.rowIndex);
     window.updateRowNumber();
     }
+    window.quoButton();
 
   }
 
   function addColumn(){
 
-    var row = document.getElementById("tblHeader");
-        if(window.columnIndex==undefined || window.columnIndex==-1)
+    let row = document.getElementById("tblHeader");
+    if(window.table.rows[0].cells.length>26)
+    {
+        alert("Cannot add more columns");
+        return;
+    }    
+    if(window.columnIndex==undefined || window.columnIndex==-1)
         {
            // let colIn = window.table.rows[0].cells.length;
            
-            var x = row.insertCell(window.table.rows[0].cells.length);
+            let x = row.insertCell(window.table.rows[0].cells.length);
             x.classList.add("tHeader");
             for(i=1; i<window.table.rows.length;i++)
             {
@@ -105,7 +145,7 @@ function addRow() {
     else
     {  
         document.getElementById("colCheck").innerHTML = "Column index is: " + window.columnIndex;
-            var x = row.insertCell(window.columnIndex+1);
+            let x = row.insertCell(window.columnIndex+1);
             x.classList.add("tHeader");
             for(i=1; i<window.table.rows.length;i++)
             {
@@ -117,11 +157,33 @@ function addRow() {
             } 
     }
     window.updateColumnHead();
+    window.quoButton();
   }
+
+  function deleteColumn()
+  {
+      if(window.table.rows[0].cells.length<=2)
+      {
+          alert("Cannot delete more columns. Atleast 1 required");
+          return;
+      }
+    for(let i=0; i<window.table.rows.length;i++)
+    {
+        row = window.table.rows[i]
+        window.table.rows[i].deleteCell(window.columnIndex);       
+    }
+    window.updateColumnHead();
+    window.quoButton();
+  }
+
+
   function updateColumnHead()
   {
-    var row = document.getElementById("tblHeader");
-    for(var i=0;i<window.table.rows.cells.length;i++)
+    let row = document.getElementById("tblHeader");
+    for(let i=1;i<window.table.rows[0].cells.length;i++)
+    {
+        window.table.rows[0].cells[i].innerHTML = String.fromCharCode(64+i);
+    }
 
   }
 
